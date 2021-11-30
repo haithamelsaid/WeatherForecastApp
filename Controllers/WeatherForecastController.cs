28da284dbd4db0128ab4331f290134d54d8ccd35
Haitham ELsaid <haithamelsaid9@gmail.com>
2021-11-30 20:19:22 +0200

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherForecastApp.Models;
+
+namespace WeatherForecastApp.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult SearchByCity()
+        {
+            var viewModel = new SearchByCity();
+            return View(viewModel);
+        }
+
+
+        public IActionResult SearchByCity(SearchByCity model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City","WeatherForecast",new {city = model.CityName});
+            }
+            return View(model);
+        }
+
+        public IActionResult City(string city)
+        {
+            var viewModel = new City();
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/SearchByCity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace WeatherForecastApp.Models
+{
+    public class SearchByCity
+    {
+        [Required(ErrorMessage ="City Name is empty!")]
+        [Display(Name ="City Name")]
+        [StringLength(30,MinimumLength =2,ErrorMessage ="Invalid Input")]
+        public string CityName { get; set; }
+        
+
+    }
+}
